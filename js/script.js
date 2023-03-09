0b5b80486a6680bb14c289229fe3502b52bfb06d
grzymala-lukasz <122287322+grzymala-lukasz@users.noreply.github.com>
2023-03-09 09:22:48 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,27 @@
+
+// ZMIENNA.addEventListener('click', FUNKCJA)
+
+document.addEventListener('DOMContentLoaded', function(){
+const nav = document.querySelector('.navbar')
+const allNavItems = document.querySelectorAll('.nav-link')
+const navList = document.querySelector('.navbar-collapse')
+
+
+
+
+function addShadow(){
+if (window.scrollY >= 300) {
+    nav.classList.add('shadow-bg')
+} else {
+    nav.classList.remove('shadow-bg')
+}
+}
+
+allNavItems.forEach(item => item.addEventListener('click', () => navList.classList.remove('show')))
+
+
+
+
+
+window.addEventListener('scroll', addShadow)
+})
